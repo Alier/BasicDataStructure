10d1e2cdf114e32f378666cfa28da48f49f9174b
alierzhang <qitianxiaosheng@yahoo.com>
2014-01-07 14:21:02 -0800

    First commit

---
+++ src/basicPackage/circularLinkedListNode.java
@@ -0,0 +1,11 @@
+package basicPackage;
+
+public class circularLinkedListNode {
+	char item;
+	circularLinkedListNode next;
+
+	public circularLinkedListNode(char node) {
+		item = node;
+		next = null;
+	}
+}
