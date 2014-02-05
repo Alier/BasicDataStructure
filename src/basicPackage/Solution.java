fa3e37dcc0e32d5a96471b7a4bec7dbf08b67066
alierzhang <qitianxiaosheng@yahoo.com>
2014-02-04 17:55:47 -0800

    added Solution for random questions

--- src/basicPackage/Solution.java
+++ src/basicPackage/Solution.java
@@ -1,6 +1,87 @@
 package basicPackage;
 
+import java.util.ArrayList;
+
 	public class Solution {
+		public Solution(){
+			super();
+		}
+		/*
+		 * Given two integers n and k, return all possible combinations of k numbers
+		 * out of 1 ... n.
+		 * 
+		 * For example, If n = 4 and k = 2, a solution is:
+		 * 
+		 * [ [2,4], [3,4], [2,3], [1,2], [1,3], [1,4], ]
+		 */
+		public ArrayList<ArrayList<Integer>> combine(int n, int k) {
+	        return rangeCombine(1,n,k);
+	    }
+	    
+		//return combination of picking any k data from startN to endN.
+	    public ArrayList<ArrayList<Integer>> rangeCombine(int startN, int endN, int k) {
+	        ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>();
+	        int countInRange = endN-startN+1;
+	        
+	        if(startN > endN || countInRange < k)
+	            return result;
+	    
+	        //count in range same as k, only one comb
+	        if(countInRange == k){
+	        	ArrayList<Integer> newList = new ArrayList<Integer>();
+	        	for(int i=startN;i<=endN;i++){
+	        		newList.add(new Integer(i));
+	        	}
+	        	result.add(newList);
+	        	return result;
+	        }
+	       
+	        //choose one, then every data in range should form one list
+	        if(k == 1){
+	        	for(int i=startN;i<=endN;i++){
+	        		ArrayList<Integer> newList = new ArrayList<Integer>();
+	        		newList.add(new Integer(i));
+	        		result.add(newList);
+	        	}
+	        	return result;
+	        }
+	        
+	        for(int i=startN;i<=endN;i++){
+	        	ArrayList<ArrayList<Integer>> restComb = rangeCombine(i+1,endN,k-1);
+	        	
+	           System.out.println("For node "+i+" Rest Combos are:");
+	           printCombs(restComb);
+	           
+	           for(int j=0;j<restComb.size();j++){
+	               ArrayList<Integer> curRestComb = restComb.get(j);
+	               curRestComb.add(new Integer(i));
+	               result.add(curRestComb);
+	           }
+	        }
+	        
+	        System.out.println("For node ["+startN+","+endN+"] All combos are:");
+	           printCombs(result);
+	           
+	        return result;
+	    }
+		
+	    public void printCombs(ArrayList<ArrayList<Integer>> comb) {
+	    	System.out.println("[");
+	    	for(int i=0;i<comb.size();i++){
+	    		ArrayList<Integer> curComb = comb.get(i);
+	    		System.out.print("[");
+	    		for(int j=0;j<curComb.size();j++){
+	    			if(j == curComb.size()-1)
+	    				System.out.print(curComb.get(j));
+	    			else
+	    				System.out.print(curComb.get(j)+",");
+	    		}
+	    		System.out.println("]");
+	    	}
+	    	System.out.println("]");
+	    }
+	    
+	    /*check if a char[][] board with filled data is valid sudoku */
 	    public boolean isValidSudoku(char[][] board) {
 	        int[] countPerNumber = new int[9];
 	        //0-8 mapping to number 1-9
@@ -75,5 +156,10 @@ package basicPackage;
 	        for(int i=0;i<counts.length;i++){
 	            counts[i] = 0;
 	        }
+	    }
+	    
+	    public static void main(String[] args) {
+	    	Solution obj = new Solution();
+	    	obj.combine(5,3);
 	    }
 	}
